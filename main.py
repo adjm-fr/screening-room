d9beb84ca0bda711f6f036af89298a95ab085dab
ADJMLyon <antoniodjm22@gmail.com>
2022-11-17 23:45:15 +0100

    Using click to handle refresh_tmdb
    Using click to handle refresh_tmdb, paths are now defined in the config json file

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import json
 import os
+import click
 
 import pandas as pd
 
 import tmdb_data_management.get_tmdb_data as tdm
 import watchlist_management.get_watchlist_infos as wm
 
-current_path = os.getcwd()
-input_path = current_path + "/data/inputs/"
-output_path = current_path + "/data/outputs/"
-refresh_tmdb = False
+@click.command()
+@click.option('--refresh_tmdb', is_flag=True, help='Do you need to refresh the TMDB database?')
+def movies_management(refresh_tmdb):
+    current_path = os.getcwd()
 
-with open(current_path + '/config.secrets.json', encoding='utf-8') as f:
-    config = json.load(f)
+    with open(current_path + '/config.secrets.json', encoding='utf-8') as f:
+        config = json.load(f)
 
-# Inputs loading
-data_ratings = pd.read_csv(input_path + "ratings.csv", encoding = 'utf8')
-data_watchlist = pd.read_csv(input_path + "WATCHLIST.csv", encoding = 'utf8')
-tmdb_data_output_path = output_path + "data_tmdb.pkl"
-if refresh_tmdb:
-    data_tmdb_df = tdm.refresh_tmdb_data(data_ratings, data_watchlist, tmdb_data_output_path, config)
-else:
-    data_tmdb_df = pd.read_pickle(tmdb_data_output_path)
-print(data_tmdb_df.shape)
+    input_path = config["path"]["input"]
+    output_path = config["path"]["output"]
 
-watchlist_output_path = output_path + "watchlist_with_tmdb.pkl"
+    # Inputs loading
+    data_ratings = pd.read_csv(input_path + "/ratings.csv", encoding = 'utf8')
+    data_watchlist = pd.read_csv(input_path + "/WATCHLIST.csv", encoding = 'utf8')
+    tmdb_data_output_path = output_path + "/data_tmdb.pkl"
+    if refresh_tmdb:
+        data_tmdb_df = tdm.refresh_tmdb_data(data_ratings, data_watchlist, tmdb_data_output_path, config)
+    else:
+        data_tmdb_df = pd.read_pickle(tmdb_data_output_path)
+    print(data_tmdb_df.shape)
 
-watchlist_df = wm.update_watchlist_with_tmdb(data_watchlist, data_tmdb_df, watchlist_output_path)
+    watchlist_output_path = output_path + "watchlist_with_tmdb.pkl"
+
+    watchlist_df = wm.update_watchlist_with_tmdb(data_watchlist, data_tmdb_df, watchlist_output_path)
+
+if __name__ == '__main__':
+    movies_management()
